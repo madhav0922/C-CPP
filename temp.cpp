0e5de45c566fae49b952b34bf27f33a5f99c6d17
Madhav <madhav@MacBook-Pro.local>
2019-11-10 00:02:17 +0530

    Linked Lists

--- temp.cpp
+++ temp.cpp
@@ -148,9 +148,85 @@ void spiralprint_inline(node *root)
     }
 }
 
+// BST two methods for checking if a tree is a BST or not
+
+// int inorder(node *root, queue<int> k)
+// {
+//     inorder(root->left);
+//     k.push(root->val);
+//     inorder(root->right);
+// }
+
+// int bst(node *root)
+// {
+//     queue<int> k;
+
+//     inorder(root, k);
+// }
+
+bool isbst(node *root, int min, int max)
+{
+    if (root == NULL)
+        return true;
+    if (root->val <= min || root->val >= max)
+        return false;
+    return (isbst(root->left, min, root->val) && isbst(root->right, root->val, max));
+}
+
+bool isbtbst(node *root)
+{
+    return isbst(root, INT16_MIN, INT16_MAX);
+}
+
+int lca_bst(node *root, int a, int b)
+{
+    if (a < root->val && b > root->val)
+        return root->val;
+    else if (a < root->val && b < root->val)
+        return lca_bst(root->left, a, b);
+    else
+        return lca_bst(root->right, a, b);
+}
+
+node *lca_binary_tree(node *root, int a, int b)
+{
+    node *right, *left;
+    if (root == NULL)
+        return NULL;
+    if (root->val == a || root->val == b)
+        return root;
+    left = lca_binary_tree(root->left, a, b);
+    right = lca_binary_tree(root->right, a, b);
+    if (left == NULL && right == NULL)
+        return NULL;
+    if (left != NULL && right != NULL)
+        return root;
+    return ((left != NULL) ? left : right);
+}
+
+void tree_left_view(node *root, int level, int *max_level)
+{
+    if (root == NULL)
+        return;
+    if (level > *max_level)
+    {
+        cout << root->val;
+        *max_level = level;
+    }
+    tree_left_view(root->left, level + 1, max_level); // do not send address of max_level again as it will result in pointer to pointer to ... case
+    tree_left_view(root->right, level + 1, max_level);
+}
+
+void left_view(node *root)
+{
+    int max_level = 0;
+    tree_left_view(root, 1, &max_level);
+}
+
 int main()
 {
-    int k;
+    int k, n;
+
     node *root1 = newnode(1);
     root1->left = newnode(2);
     root1->right = newnode(3);
@@ -160,6 +236,7 @@ int main()
     root1->right->right = newnode(7);
     root1->left->left->right = newnode(8);
     root1->right->right->left = newnode(9);
+
     node *root2 = newnode(1);
     root2->left = newnode(2);
     root2->right = newnode(3);
@@ -169,6 +246,40 @@ int main()
     root2->right->right = newnode(7);
     root2->left->left->right = newnode(8);
     root2->right->right->left = newnode(9);
+
+    // for BST
+    node *root3 = newnode(10);
+    root3->left = newnode(5);
+    root3->right = newnode(15);
+    root3->left->left = newnode(3);
+    root3->left->right = newnode(7);
+    root3->right->left = newnode(13);
+    root3->right->right = newnode(20);
+    root3->left->left->left = newnode(1);
+    root3->right->right->right = newnode(25);
+
+    //LCA Binary Tree
+    node *root4 = newnode(3);
+    root4->left = newnode(6);
+    root4->right = newnode(8);
+    root4->left->left = newnode(2);
+    root4->left->right = newnode(11);
+    root4->right->right = newnode(13);
+    root4->left->right->left = newnode(9);
+    root4->left->right->right = newnode(5);
+    root4->right->right->left = newnode(7);
+
+    //left view , right view
+    node *root5 = newnode(3);
+    root5->left = newnode(6);
+    root5->right = newnode(8);
+    root5->left->left = newnode(2);
+    root5->left->right = newnode(11);
+    root5->right->right = newnode(13);
+    root5->left->right->left = newnode(9);
+    root5->left->right->right = newnode(5);
+    root5->right->right->left = newnode(7);
+    root5->right->right->left->right = newnode(40);
 
     cout << "\n"
          << "no of leaves"
@@ -218,5 +329,31 @@ int main()
          << "\n";
 
     spiralprint_inline(root1); // reverse level order inline
-    return 0;
+
+    n = isbtbst(root3);
+    cout << "\n"
+         << "is bst? " << k
+         << "\n";
+
+    //LCA
+    int a = 1, b = 7;
+    n = lca_bst(root3, a, b);
+    cout << "\n"
+         << "LCA: " << n
+         << "\n";
+
+    // LCA Binary Tree
+    int c = 9, d = 7;
+    node *lcabt;
+    lcabt = lca_binary_tree(root4, c, d);
+    cout << "\n"
+         << "LCA_Binary_Tree: " << lcabt->val
+         << "\n";
+
+    // left view of tree
+    cout << "\n"
+         << "Left View of Tree: "
+         << "\n";
+    left_view(root5);
+    cout << "\n";
 }
